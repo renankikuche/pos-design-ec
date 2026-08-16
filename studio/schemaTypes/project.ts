b638672d5af2e245d4380fd01ba6cc433764d02b
renankikuche <renan.kikuche@gmail.com>
2026-08-16 20:23:47 -0300

    adiciona schema de projeto

---
+++ studio/schemaTypes/project.ts
@@ -0,0 +1,51 @@
+import { defineArrayMember, defineField, defineType } from 'sanity'
+import { ProjectsIcon } from '@sanity/icons/Projects'
+
+export const project = defineType({
+  name: 'project',
+  title: 'Projeto',
+  type: 'document',
+  icon: ProjectsIcon,
+  fields: [
+    defineField({
+      name: 'name',
+      title: 'Nome',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: { source: 'name' },
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'description',
+      title: 'Descrição',
+      type: 'array',
+      of: [defineArrayMember({ type: 'block' })],
+    }),
+    defineField({
+      name: 'gallery',
+      title: 'Galeria de Imagens',
+      type: 'array',
+      of: [
+        defineArrayMember({
+          type: 'image',
+          options: { hotspot: true },
+          fields: [
+            defineField({
+              name: 'alt',
+              title: 'Texto alternativo',
+              type: 'string',
+            }),
+          ],
+        }),
+      ],
+    }),
+  ],
+  preview: {
+    select: { title: 'name', media: 'gallery.0' },
+  },
+})
